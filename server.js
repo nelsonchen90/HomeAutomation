508b388f4ea98ba9fe7bc44ca7e37e9eae14df93
Nelson <nelsonchen90@gmail.com>
2020-12-17 16:54:37 +0000

    add helmet for security

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-import express from'express';
+import express from 'express';
+import helmet from 'helmet';
 import mainRoute from './routes/mainRoute.js';
 
 const app = express();
 const host = '0.0.0.0';
 const port = 3000;
 
+app.use(helmet());
 app.use('/', mainRoute);
 
 app.listen(port, host, () => {
