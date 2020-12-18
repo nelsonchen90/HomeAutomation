04ec8af7f9ae248becef074bb5f9c3e7a34e4670
Nelson <nelsonchen90@gmail.com>
2020-12-18 17:10:53 +0000

    https

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
 #!/home/pi/.nvm/versions/node/v15.4.0/bin/node
 
-import express from 'express';
+import express from 'express'
 // import helmet from 'helmet';
-import mainRoute from './routes/mainRoute.js';
+import fs from 'fs'
+import https from 'https'
+import path from 'path'
+import mainRoute from './routes/mainRoute.js'
 
-const app = express();
-const host = '0.0.0.0';
-const isProd = process.env.NODE_ENV === 'production';
-const port = isProd ? 3002 : 3000;
-const appName = 'Home automation';
-
+const app = express()
+const host = '0.0.0.0'
+const isProd = process.env.NODE_ENV === 'production'
+const httpsPort = isProd ? 443 : 3000
+const appName = 'Home automation'
 
 // app.use(helmet());
-app.set('title', appName);
-app.use('/', mainRoute);
+app.set('title', appName)
+app.use('/', mainRoute)
 
-const server = app.listen(port, host, () => {
-  console.log(`${appName} listening at http://${host}:${port}`)
-  console.log('pid is ' + process.pid);
-});
+const server = https.createServer({
+  key: fs.readFileSync(path.join(path.resolve(), 'server.key')),
+  cert: fs.readFileSync(path.join(path.resolve(), 'server.cert'))
+}, app)
+
+server.listen(httpsPort, host, () => {
+  console.log(`${appName} listening at https://${host}:${httpsPort}`)
+  console.log('pid is ' + process.pid)
+})
 
 const handleExit = (signal) => {
-  console.log(`Received ${signal}. Close my server properly.`);
-  server.close(function () {
-    process.exit(0);
-  });
+  console.log(`Received ${signal}. Close my server properly.`)
+  server.close(() => {
+    // better way to terminate
+    process.exit(0)
+  })
 }
-process.on('SIGINT', handleExit);
-process.on('SIGQUIT', handleExit);
-process.on('SIGTERM', handleExit);
+process.on('SIGINT', handleExit)
+process.on('SIGQUIT', handleExit)
+process.on('SIGTERM', handleExit)
