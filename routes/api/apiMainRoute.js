bda32778c145d44fddc1fa974587470740222a1d
Nelson <nelsonchen90@gmail.com>
2020-12-19 18:26:11 +0000

    better error handling on api

--- routes/api/apiMainRoute.js
+++ routes/api/apiMainRoute.js
@@ -3,7 +3,14 @@ import USBSwitchRouter from './USBSwitch.js'
 
 const apiRouter = express.Router()
 
-apiRouter.use('*', express.json())
+apiRouter.use('/', express.json())
 apiRouter.use('/usbSwitch', USBSwitchRouter)
+apiRouter.use('*', (req, res) => {
+  console.log('Unknown api request')
+  console.log(`[${req.method}] ${req.baseUrl}`)
+  console.log(`From ip: ${req.ip}`)
+  res.statusCode = 404
+  res.send('Unknown api endpoint, please check usage')
+})
 
 export default apiRouter
