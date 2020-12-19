11a68db088044089ac967ad902c6a15daa88b54f
Nelson <nelsonchen90@gmail.com>
2020-12-19 02:31:59 +0000

    http for local

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import mainRoute from './routes/mainRoute.js'
 const app = express()
 const host = '0.0.0.0'
 const isProd = process.env.NODE_ENV === 'production'
-const port = 80
+const port = isProd ? 80 : 3000
 const httpsPort = isProd ? 443 : 3001
 const appName = 'Home automation'
 
@@ -19,32 +19,32 @@ app.use(helmet())
 app.set('title', appName)
 app.use('/', mainRoute)
 
-let exposedServer
+let securedServer
 if (isProd) {
-  exposedServer = http.createServer(app)
-  exposedServer.listen(port, host, () => {
-    console.log(`${appName} listening at http://${host}:${port}`)
+  const securedServer = https.createServer({
+    key: fs.readFileSync(path.join(path.resolve(), 'privkey.pem')),
+    cert: fs.readFileSync(path.join(path.resolve(), 'fullchain.pem'))
+  }, app)
+  securedServer.listen(httpsPort, host, () => {
+    console.log(`${appName} listening at https://${host}:${httpsPort}`)
     console.log('pid is ' + process.pid)
   })
 }
 
-const securedServer = https.createServer({
-  key: fs.readFileSync(path.join(path.resolve(), 'privkey.pem')),
-  cert: fs.readFileSync(path.join(path.resolve(), 'fullchain.pem'))
-}, app)
-securedServer.listen(httpsPort, host, () => {
-  console.log(`${appName} listening at https://${host}:${httpsPort}`)
+const exposedServer = http.createServer(app)
+exposedServer.listen(port, host, () => {
+  console.log(`${appName} listening at http://${host}:${port}`)
   console.log('pid is ' + process.pid)
 })
 
 const handleExit = (signal) => {
   console.log(`Received ${signal}. Close my server properly.`)
-  securedServer.close(() => {
+  exposedServer.close(() => {
     // better way to terminate
     process.exit(0)
   })
-  if (exposedServer) {
-    exposedServer.close(() => {
+  if (securedServer) {
+    securedServer.close(() => {
       // better way to terminate
       process.exit(0)
     })
