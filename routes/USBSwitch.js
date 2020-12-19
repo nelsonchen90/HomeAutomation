da984c4e4fcc56b5ebdb68d14e4e596e1a5075e9
Nelson <nelsonchen90@gmail.com>
2020-12-19 00:57:05 +0000

    lint

--- routes/USBSwitch.js
+++ routes/USBSwitch.js
@@ -1,34 +1,33 @@
-import express from 'express';
-import usbSwitch from '../utils/usbSwitch.js';
+import express from 'express'
+import usbSwitch from '../utils/usbSwitch.js'
 
-const USBSwitchRouter = express.Router();
+const USBSwitchRouter = express.Router()
 
 USBSwitchRouter.get('/:value', (req, resp, next) => {
-  next();
-});
+  next()
+})
 
 USBSwitchRouter.param('value', (req, resp, next, value) => {
-  let powerFlag;
+  let powerFlag
   switch (value) {
     case 'on':
-      powerFlag = true;
-      break;
+      powerFlag = true
+      break
     case 'off':
-      powerFlag = false;
-      break;
+      powerFlag = false
+      break
     case 'status':
-      powerFlag = 'status';
+      powerFlag = 'status'
   }
   usbSwitch(powerFlag, (stdout, stderr, error) => {
     if (error) {
-      resp.send(`Error: \n ${error}`);
-      resp.statusCode = 500;
+      resp.send(`Error: \n ${error}`)
+      resp.statusCode = 500
     } else {
-      resp.send(`USB ${powerFlag}: \n ${stdout}\n`);
-      resp.statusCode = 200;
+      resp.send(`USB ${powerFlag}: \n ${stdout}\n`)
+      resp.statusCode = 200
     }
-  });
-});
+  })
+})
 
-
-export default USBSwitchRouter;
+export default USBSwitchRouter
